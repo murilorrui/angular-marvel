00284e891a7dc74d458517b86c633c4a7c8a4a91
Murilo Henrique Ribeiro Rui <murilo.rui@MacBook-Pro.local>
2022-05-28 01:23:24 -0300

    feat: add layout components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { PageTemplateComponent } from './layout/page-template/page-template.component';
+import { NavComponent } from './layout/nav/nav.component';
+import { HeroComponent } from './page/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageTemplateComponent,
+    NavComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
